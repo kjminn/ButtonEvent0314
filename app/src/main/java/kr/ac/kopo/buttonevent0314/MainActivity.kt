9a56a01faaf062daba8f6f985b604cd59981a1a8
kjminn <kjminn2001@gmail.com>
2024-03-14 16:23:04 +0900

    4개의 버튼 이벤트 처리

--- app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btn911 : Button
     lateinit var btnGallery : Button
     lateinit var btnFinish : Button
+    lateinit var btnListener : View.OnClickListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,24 +24,22 @@ class MainActivity : AppCompatActivity() {
         btnGallery = findViewById(R.id.buttonGallery)
         btnFinish = findViewById(R.id.buttonFinish)
 
-        btnHome.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu"))
-            startActivity(intent)
-        }
-        btn911.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
+        btnListener = View.OnClickListener {
+            when(it.id){
+                R.id.buttonHome -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu"))
+                R.id.button911 -> Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
+                R.id.buttonGallery -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
+            }
             startActivity(intent)
         }
 
-        btnGallery.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
-            startActivity(intent)
-        }
+        btnHome.setOnClickListener(btnListener)
+        btn911.setOnClickListener(btnListener)
+        btnGallery.setOnClickListener(btnListener)
 
         btnFinish.setOnClickListener {
             finish()
         }
-
 
         btn.setOnClickListener {
             Toast.makeText(applicationContext, "기분 좋은 오후입니다.",Toast.LENGTH_SHORT).show()
